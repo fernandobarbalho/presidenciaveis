7b191b04db2720e11f9c354b17e6c72d0381f4e7
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2022-01-10 07:05:31 -0300

    Inclusão das análises sobre lula e moro

---
+++ tweets_lula.R
@@ -0,0 +1,43 @@
+#Para capturar apenas as principais hashtags que citam lula
+
+analise_lula<-
+  analise_hashtags(lula,10)
+
+
+#Para fazer tf_idf (caminho completo)
+
+tf_idf_lula<- 
+  associa_tipo_mensagem(lula, 
+                        n= 3300,
+                        tags_positivas = analise_lula$hashtag[c(2,4,5,9)], 
+                        tags_negativas = analise_lula$hashtag[c(3,10)],
+                        #usuarios_noticias = imprensa,
+                        tipo =2) %>%
+  processa_tweets() %>%
+  tabela_tf_idf()
+
+#Para mostrar o gráfico tf_idf (caminho curto)
+
+tf_idf_lula$graph
+
+
+#para gerar a base do modelo ml (caminho completo)
+
+modelo_ml_lula<-
+  associa_tipo_mensagem(lula, 
+                        n= 3300,
+                        tags_positivas = analise_lula$hashtag[c(2,4,5,9)], 
+                        tags_negativas = analise_lula$hashtag[c(3,10)],
+                        #usuarios_noticias = imprensa,
+                        tipo =2) %>%
+  processa_tweets() %>%
+  prepara_base_ml() 
+
+#para gerar o modelo ml (caminho curto)
+
+dt_lula<-
+modelo_ml_lula %>%
+  cria_arvore_decisao()
+
+#Para gerar a figura da dt (caminho curto)
+grafico_arvore_decisao(dt_lula)
---
+++ tweets_moro.R
@@ -0,0 +1,43 @@
+#Para capturar apenas as principais hashtags que citam moro
+
+analise_moro<-
+  analise_hashtags(moro,10)
+
+
+#Para fazer tf_idf (caminho completo)
+
+tf_idf_moro<- 
+  associa_tipo_mensagem(moro, 
+                        n= 3300,
+                        tags_positivas = analise_moro$hashtag[c(3,5,8,12)], 
+                        tags_negativas = analise_moro$hashtag[c(1,6,7)],
+                        #usuarios_noticias = imprensa,
+                        tipo =2) %>%
+  processa_tweets() %>%
+  tabela_tf_idf()
+
+#Para mostrar o gráfico tf_idf (caminho curto)
+
+tf_idf_moro$graph
+
+
+#para gerar a base do modelo ml (caminho completo)
+
+modelo_ml_moro<-
+  associa_tipo_mensagem(moro, 
+                        n= 3300,
+                        tags_positivas = analise_moro$hashtag[c(3,5,8,12)], 
+                        tags_negativas = analise_moro$hashtag[c(1,6,7)],
+                        #usuarios_noticias = imprensa,
+                        tipo =2) %>%
+  processa_tweets() %>%
+  prepara_base_ml() 
+
+#para gerar o modelo ml (caminho curto)
+
+dt_moro<-
+modelo_ml_moro %>%
+  cria_arvore_decisao()
+
+#Para gerar a figura da dt (caminho curto)
+grafico_arvore_decisao(dt_moro)
